7d353ee2adae664cb8f8c8207e96ad4dc2f4367c
Felipe Castro Richter <felipe@digivoro.com>
2020-12-15 18:29:25 -0300

    Añado validación

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -2,48 +2,128 @@
   <form class="contactform" @submit.prevent="handleSubmit">
     <v-container>
       <v-row>
-        <v-col class="form-input" cols="12" md="6">
-          <label class="form-input__label" for="firstname">NOMBRE</label>
+        <!-- Firstname -->
+        <v-col
+          class="form-group"
+          :class="{ 'form-group--error': $v.formInfo.firstname.$error }"
+          cols="12"
+          md="6"
+        >
+          <label class="form__label" for="firstname">NOMBRE</label>
           <input
-            class="form-input__field"
+            v-model.trim="$v.formInfo.firstname.$model"
+            class="form__field"
             v-model="formInfo.firstname"
             type="text"
             id="firstname"
             name="firstname"
           />
+          <div class="error" v-if="!$v.formInfo.firstname.minLength">
+            El nombre debe tener al menos
+            {{ $v.formInfo.firstname.$params.minLength.min }} letras.
+          </div>
+          <div
+            class="error"
+            v-if="
+              formInfo.submitStatus === 'ERROR' &&
+                !$v.formInfo.firstname.required
+            "
+          >
+            El nombre es requerido.
+          </div>
         </v-col>
 
-        <v-col cols="12" md="6">
-          <label class="form-input__label" for="lastname">APELLIDO</label>
+        <!-- Lastname -->
+        <v-col
+          class="form-group"
+          :class="{ 'form-group--error': $v.formInfo.lastname.$error }"
+          cols="12"
+          md="6"
+        >
+          <label class="form__label" for="lastname">APELLIDO</label>
           <input
-            class="form-input__field"
+            class="form__field"
+            v-model.trim="$v.formInfo.lastname.$model"
             v-model="formInfo.lastname"
             type="text"
             id="lastname"
             name="lastname"
           />
+          <div class="error" v-if="!$v.formInfo.lastname.minLength">
+            El apellido debe tener al menos
+            {{ $v.formInfo.lastname.$params.minLength.min }} letras.
+          </div>
+          <div
+            class="error"
+            v-if="
+              formInfo.submitStatus === 'ERROR' &&
+                !$v.formInfo.lastname.required
+            "
+          >
+            El apellido es requerido.
+          </div>
         </v-col>
 
-        <v-col cols="12" md="6">
-          <label class="form-input__label" for="email">EMAIL</label>
+        <!-- Email -->
+        <v-col
+          class="form-group"
+          :class="{ 'form-group--error': $v.formInfo.email.$error }"
+          cols="12"
+          md="6"
+        >
+          <label class="form__label" for="email">EMAIL</label>
           <input
-            class="form-input__field"
+            class="form__field"
+            v-model.trim="$v.formInfo.email.$model"
             v-model="formInfo.email"
             type="email"
             id="email"
             name="email"
           />
+          <div class="error" v-if="!$v.formInfo.email.email">
+            Ingresa un email válido.
+          </div>
+          <div
+            class="error"
+            v-if="
+              formInfo.submitStatus === 'ERROR' && !$v.formInfo.email.required
+            "
+          >
+            El email es requerido.
+          </div>
         </v-col>
 
-        <v-col cols="12" md="6">
-          <label class="form-input__label" for="phone">TELÉFONO</label>
+        <!-- Phone -->
+        <v-col
+          class="form-group"
+          :class="{ 'form-group--error': $v.formInfo.phone.$error }"
+          cols="12"
+          md="6"
+        >
+          <label class="form__label" for="phone">TELÉFONO</label>
           <input
-            class="form-input__field"
+            class="form__field"
+            v-model.trim="$v.formInfo.phone.$model"
             v-model="formInfo.phone"
             type="tel"
             id="phone"
             name="phone"
           />
+          <div class="error" v-if="!$v.formInfo.phone.minLength">
+            El teléfono debe tener al menos
+            {{ $v.formInfo.phone.$params.minLength.min }} números.
+          </div>
+          <div class="error" v-if="!$v.formInfo.phone.numeric">
+            Solo se aceptan números.
+          </div>
+          <div
+            class="error"
+            v-if="
+              formInfo.submitStatus === 'ERROR' && !$v.formInfo.phone.required
+            "
+          >
+            El teléfono es requerido.
+          </div>
         </v-col>
 
         <v-col cols="12" class="text-right">
@@ -56,24 +136,53 @@
 
 <script>
 import { mapActions } from "vuex";
+import { required, numeric, email, minLength } from "vuelidate/lib/validators";
 
 export default {
   data() {
     return {
       formInfo: {
-        firstname: null,
-        lastname: null,
-        email: null,
-        phone: null
+        firstname: "",
+        lastname: "",
+        email: "",
+        phone: "",
+        submitStatus: null
       }
     };
+  },
+
+  validations: {
+    formInfo: {
+      firstname: {
+        required,
+        minLength: minLength(3)
+      },
+      lastname: {
+        required,
+        minLength: minLength(3)
+      },
+      email: {
+        required,
+        email
+      },
+      phone: {
+        required,
+        numeric,
+        minLength: minLength(9)
+      }
+    }
   },
 
   methods: {
     ...mapActions(["newsletterSubscribe"]),
 
     handleSubmit() {
-      this.newsletterSubscribe(this.formInfo);
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        this.formInfo.submitStatus = "ERROR";
+      } else {
+        this.newsletterSubscribe(this.formInfo);
+      }
     }
   }
 };
